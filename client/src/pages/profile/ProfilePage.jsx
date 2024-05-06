f87ef1e591809decc1f5b35ff8922baad6f301fc
Abhishek Katkam <abhishekkatkam30@gmail.com>
2024-05-06 18:18:25 +0530

    Started profile page ui

--- client/src/pages/profile/ProfilePage.jsx
+++ client/src/pages/profile/ProfilePage.jsx
@@ -6,15 +6,23 @@ const ProfilePage = () => {
     <>
       <div className="flex-[4_4_0] mr-auto border-r border-slate-300 dark:border-slate-500 min-h-screen p-4">
         <div className="w-full h-full flex flex-col items-start">
-          <div className="w-full h-[62px] flex items-center justify-start border-b border-slate-300 dark:border-slate-500">
-            <h1 className="text-2xl font-semibold capitalize leading-snug tracking-tight">
-              Profile
-            </h1>
+          <div className="w-full h-[415px] flex flex-col items-center justify-start border-b border-slate-300 dark:border-slate-500">
+            <div className="w-full h-[200px] relative">
+              <div className="w-full h-full bg-slate-200 dark:bg-slate-600">
+                <img
+                  src="https://images.unsplash.com/photo-1528465424850-54d22f092f9d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="absolute w-40 h-40 rounded-full bg-slate-400 z-10 -bottom-20 left-4 border-4 border-slate-200 dark:border-slate-300">
+                <img src="/avatar-placeholder.png" alt="" className="w-full h-full object-cover rounded-full" />
+              </div>
+            </div>
           </div>
 
-          <div className="mt-6 w-full overflow-x-auto p-4 pt-3">
-            <div className="m-auto flex w-full max-w-2xl flex-col items-center space-y-6"></div>
-          </div>
+          {/* personal feed */}
+          <div></div>
         </div>
       </div>
       <WhoToFollow isHomePage={true} />
